11bf0a924e700cf76e993488ae17dfc163b1e3c5
Nick Perry <nick.perry12@outlook.com>
2022-09-06 21:43:23 -0700

    This is the calculator program

---
+++ Lesson_2/calculator.rb
@@ -0,0 +1,29 @@
+# Define the method to calculate
+
+def calculate(operation, number_one, number_two)
+  if operation == 'add'
+    number_one + number_two
+  elsif operation == 'subtract'
+    number_one - number_two
+  elsif operation == 'multiply'
+    number_one * number_two
+  elsif operation == 'divide'
+    number_one / number_two
+  end
+end
+
+# First gather user input for numbers to be calculated
+Kernel.puts("> Welcome to the calculator!")
+
+Kernel.puts("> Please enter the first number of the equation:")
+number_one = Kernel.gets().chomp().to_i()
+
+Kernel.puts("> Please enter the second number of the equation")
+number_two = Kernel.gets().chomp().to_i()
+
+# Next, ask the user to which operation they would like to perform
+
+Kernel.puts("> Please enter which operation you would like to perform: \n  add, subtract, multiply, or divide")
+operation = Kernel.gets().chomp().downcase
+
+puts "The result is #{calculate(operation, number_one, number_two)!}"
