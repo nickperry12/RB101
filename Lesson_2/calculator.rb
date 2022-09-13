89e4b7fdc8979a41b59f9a6ed9b0ec27194830d4
Nick Perry <nick.perry12@outlook.com>
2022-09-12 22:07:17 -0700

    refactored calculator

--- Lesson_2/calculator.rb
+++ Lesson_2/calculator.rb
@@ -1,29 +1,117 @@
+# method to style our prompts
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
 # Define the method to calculate
 
-def calculate(operation, number_one, number_two)
-  if operation == 'add'
+def calculate(operator, number_one, number_two)
+  case operator
+  when '1'
     number_one + number_two
-  elsif operation == 'subtract'
+  when '2'
     number_one - number_two
-  elsif operation == 'multiply'
+  when '3'
     number_one * number_two
-  elsif operation == 'divide'
+  when '4'
     number_one / number_two
   end
 end
 
+# this method will display our operation of choice in a message
+
+def operator_to_message(operator)
+  case operator
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+# This method will check if the number is valid
+
+def valid_number?(num)
+  num.to_f != 0
+end
+
 # First gather user input for numbers to be calculated
-Kernel.puts("> Welcome to the calculator!")
 
-Kernel.puts("> Please enter the first number of the equation:")
-number_one = Kernel.gets().chomp().to_f()
+prompt("Welcome to the Calculator! Enter your name:")
 
-Kernel.puts("> Please enter the second number of the equation")
-number_two = Kernel.gets().chomp().to_f()
+name = ''
+loop do
+  name = Kernel.gets().chomp()
 
-# Next, ask the user to which operation they would like to perform
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
 
-Kernel.puts("> Please enter which operation you would like to perform: \n  add, subtract, multiply, or divide")
-operation = Kernel.gets().chomp().downcase
+prompt("Hi #{name}!")
 
-puts "The result is #{calculate(operation, number_one, number_two)}!"
+loop do # main loop
+  number_one = ''
+  loop do
+    prompt("Please enter the first number of the equation:")
+    number_one = Kernel.gets().chomp().to_f
+
+    if valid_number?(number_one)
+      break
+    else
+      prompt("That number is not valid. Please enter a valid number.")
+    end
+  end
+
+  number_two = ''
+  loop do
+    prompt("Please enter the second number of the equation:")
+    number_two = Kernel.gets().chomp().to_f
+
+    if valid_number?(number_two)
+      break
+    else
+      prompt("That number is not valid. Please enter a valid number.")
+    end
+  end
+
+  # Next, ask the user to which operation they would like to perform
+
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Please enter a valid operator. Must choose 1, 2, 3, or 4.")
+    end
+  end
+
+  prompt("#{operator_to_message(operator)} the two numbers...")
+
+  prompt("The result is #{calculate(operator, number_one, number_two)}!")
+
+  prompt("Do you want to perform another calculation? ('Y' or 'N')")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the Calculator! Good-bye.")
