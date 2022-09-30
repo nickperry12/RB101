8a14619457bdfcc3311a5ce8cea5b17e7aafc5f6
Nick Perry <nick.perry12@outlook.com>
2022-09-29 17:58:38 -0700

    rps game udated

--- Lesson_2/rps.rb
+++ Lesson_2/rps.rb
@@ -7,8 +7,9 @@ def prompt(string)
 end
 
 def valid_input?(input)
-  input.downcase == 'rock' || input.downcase == 'scissors' ||
-    input.downcase == 'paper'
+  input == 'rock' || input == 'scissors' ||
+    input == 'paper' || input == 'p' || input == 'r' ||
+    input == 's'
 end
 
 # array for CPU choices
@@ -42,7 +43,7 @@ while in_game
   cpu_choice = cpu_options.sample
 
   loop do
-    player_choice = gets.chomp
+    player_choice = gets.chomp.downcase
     if valid_input?(player_choice) == false
       prompt(MSG['invalid_input'])
     else
@@ -51,35 +52,44 @@ while in_game
   end
 
   # player outcomes if they choose rock
-  if player_choice == 'rock' && cpu_choice == 'paper'
+  if (player_choice == 'rock' || player_choice == 'r')\
+    && cpu_choice == 'paper'
     prompt("CPU Wins!")
   end
-  if player_choice == 'rock' && cpu_choice == 'scissors'
+  if (player_choice == 'rock' || player_choice == 'r')\
+    && cpu_choice == 'scissors'
     prompt("#{username} wins!")
   end
-  if player_choice == 'rock' && cpu_choice == 'rock'
+  if (player_choice == 'rock' || player_choice == 'r')\
+    && cpu_choice == 'rock'
     prompt("Stalemate! No one wins!")
   end
 
   # player outcomes if they choose paper
-  if player_choice == 'paper' && cpu_choice == 'paper'
+  if (player_choice == 'paper' || player_choice == 'p')\
+    && cpu_choice == 'paper'
     prompt("Stalemate! No one wins!")
   end
-  if player_choice == 'paper' && cpu_choice == 'scissors'
+  if (player_choice == 'paper' || player_choice == 'p')\
+    && cpu_choice == 'scissors'
     prompt("CPU Wins!")
   end
-  if player_choice == 'paper' && cpu_choice == 'rock'
+  if (player_choice == 'paper' || player_choice == 'p')\
+    && cpu_choice == 'rock'
     prompt("#{username} wins!")
   end
 
   # player outcomes if they choose scissors
-  if player_choice == 'scissors' && cpu_choice == 'rock'
+  if (player_choice == 'scissors' || player_choice == 's')\
+    && cpu_choice == 'rock'
     prompt("CPU Wins!")
   end
-  if player_choice == 'scissors' && cpu_choice == 'paper'
+  if (player_choice == 'scissors' || player_choice == 's')\
+    && cpu_choice == 'paper'
     prompt("#{username} wins!")
   end
-  if player_choice == 'scissors' && cpu_choice == 'scissors'
+  if (player_choice == 'scissors' || player_choice == 's')\
+    && cpu_choice == 'scissors'
     prompt("Stalemate! No one wins!")
   end
 
